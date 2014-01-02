25f9e111a249c1d749db88b6434ee0f892dc5498
Hao-Ding <dinghao_nwpu@hotmail.com>
2014-01-03 00:03:15 +0800

    add comments

--- myString/MyString.h
+++ myString/MyString.h
@@ -13,12 +13,12 @@ using namespace std;
 class MyString
 {
 public:
-	MyString(const char *str = NULL);
-	MyString(const MyString &other);
-	~MyString();
-	MyString &operator= (const MyString &other);
-	friend ostream &operator<< (ostream &os, const MyString &str);
-	size_t getLength() const;
+	MyString(const char *str = NULL);	// constructor
+	MyString(const MyString &other);	// copy constructor
+	~MyString();						// destructor
+	MyString &operator= (const MyString &other);	// overload of =
+	friend ostream &operator<< (ostream &os, const MyString &str);	//overload of <<
+	size_t getLength() const;	// get the length of string
 private:
 	char *m_data;
 };

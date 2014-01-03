b5279c343299d40b77366c6f923c58a529850f56
Hao-Ding <dinghao_nwpu@hotmail.com>
2014-01-03 22:34:01 +0800

    add getSub, c_str, isExist, ==

--- myString/MyString.cpp
+++ myString/MyString.cpp
@@ -13,24 +13,40 @@ using namespace std;
 
 MyString::MyString(const char *str) {
 	m_data = NULL;
-	if (str != NULL) {
-		m_data = new char[strlen(str) + 1];
-		if (m_data != NULL) {
+	if (str != NULL) {	// check if str is empty
+		m_data = new char[strlen(str) + 1];	// allocate memory
+		if (m_data != NULL) {	// check if allocation succeeds
 			strcpy(m_data, str);
 		}
 	}
-	else {
+	else {	// str is empty
 		m_data = new char[1];
 		m_data[0] = '\0';
 	}
 }
 
-MyString::MyString(const MyString &other) {
+MyString::MyString(const char ch, size_t n) {
 	m_data = NULL;
-	if (other.m_data != NULL) {
-		m_data = new char[strlen(other.m_data) + 1];
-		if (m_data != NULL) {
-			strcpy(m_data, other.m_data);
+	if (n > 0) { // check if the length is positive
+		m_data = new char[n];
+		if (m_data != NULL) {	// check if allocation succeeds
+			for (int i = 0; i < n; i++) {
+				m_data[i] = ch;
+			}
+		}
+	}
+	else {	// n is 0 or negative
+		m_data = new char[1];
+		m_data[0] = '\0';
+	}
+}
+
+MyString::MyString(const MyString &rStr) {
+	m_data = NULL;
+	if (rStr.m_data != NULL) {	// check if rStr is empty
+		m_data = new char[strlen(rStr.m_data) + 1];	// allocate mem
+		if (m_data != NULL) {	// check if allocation succeeds
+			strcpy(m_data, rStr.m_data);
 		}
 	}
 }
@@ -42,18 +58,77 @@ MyString::~MyString() {
 	}
 }
 
-MyString &MyString::operator=(const MyString &other) {
-	if (this != &other) {
-		if (m_data != NULL) {
+size_t MyString::getLength() const {
+	return strlen(m_data);
+}
+
+void MyString::getSub(const size_t start, const size_t end, char *subStr) {
+	for (size_t i = 0; i < end - start; i++) {
+		subStr[i] = m_data[start + i];
+	}
+	subStr[end - start] = '\0';
+	return;
+}
+
+void MyString::c_str(char *str) {
+	for (size_t i = 0; i < strlen(m_data) + 1; i++){
+		str[i] = m_data[i];
+	}
+	return;
+}
+
+bool MyString::isExist(const char ch) const{
+	for (size_t i = 0; i < strlen(m_data); i++) {
+		if ( ch == m_data[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool MyString::isExist(const char *rStr){
+	for (size_t i = 0; i < strlen(m_data); i++) {
+		char *tmpStr = new char[strlen(rStr)+1];
+		getSub(i, i + strlen(rStr), tmpStr);
+		if (strcmp(rStr, tmpStr) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool MyString::isExist(const MyString &subStr){
+	for (size_t i = 0; i < strlen(m_data); i++) {
+		char *tmpStr = new char[strlen(subStr.m_data)+1];
+		getSub(i, i + strlen(subStr.m_data), tmpStr);
+		if (strcmp(subStr.m_data, tmpStr) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+MyString &MyString::operator=(const MyString &rStr) {
+	if (this != &rStr) {	// check if this equal to &rStr
+		if (m_data != NULL) {	// if m_data is not empty
 			delete[] m_data;
 			m_data = NULL;
 		}
-		if (other.m_data != NULL) {
-			m_data = new char[strlen(other.m_data) + 1];
-			strcpy(m_data, other.m_data);
+		if (rStr.m_data != NULL) {	// check if rStr is empty
+			m_data = new char[strlen(rStr.m_data) + 1];
+			if (m_data != NULL) { // check if allocation succeeds
+				strcpy(m_data, rStr.m_data);
+			}
 		}
 	}
 	return *this;
+}
+
+bool MyString::operator==(const MyString &rStr) const {
+	if (strcmp(m_data, rStr.m_data) == 0) {
+		return true;
+	}
+	return false;
 }
 
 ostream &operator<<(ostream &os, const MyString &str) {
@@ -61,6 +136,3 @@ ostream &operator<<(ostream &os, const MyString &str) {
 	return os;
 }
 
-size_t MyString::getLength() const {
-	return strlen(m_data);
-}
--- myString/MyString.h
+++ myString/MyString.h
@@ -14,11 +14,21 @@ class MyString
 {
 public:
 	MyString(const char *str = NULL);	// constructor
-	MyString(const MyString &other);	// copy constructor
+	MyString(const char ch, const size_t n);	//constructor from ch*n
+	MyString(const MyString &rStr);	// copy constructor
 	~MyString();						// destructor
-	MyString &operator= (const MyString &other);	// overload of =
-	friend ostream &operator<< (ostream &os, const MyString &str);	//overload of <<
+
 	size_t getLength() const;	// get the length of string
+	void getSub(const size_t start, const size_t end, char *subStr);	// get subString from start to end-1; min of start is 0; end > start
+	void c_str(char *str);	// transfer to c-style char array
+	bool isExist(const char ch) const;	// check whether ch is existed
+	bool isExist(const char *rStr);	// check whether str is existed
+	bool isExist(const MyString &subStr);	// check whether subStr is existed
+
+	MyString &operator=(const MyString &rStr);	// overload of =
+	bool operator==(const MyString &rStr) const;	// overload of ==
+	friend ostream &operator<<(ostream &os, const MyString &str);	//overload of <<
+//	friend MyString &operator+ (, const MyString &str);	//overload of <<
 private:
 	char *m_data;
 };

780e12e69b3bd4389579c2d8b2d033b5a77008f5
Hao-Ding <dinghao_nwpu@hotmail.com>
2014-01-25 03:16:42 +0800

    test static

---
+++ tmp/hello.c
@@ -0,0 +1,10 @@
+/********************************************************
+ * Author		: Hao Ding
+ * Email		: mailtodinghao@gmail.com
+ * Created Time	: Sat 25 Jan 2014 03:08:46 AM CST
+ * Filename		: hello.c
+ * Description	: 
+ * ******************************************************/
+int a = 10;
+static int b = 11;
+
---
+++ tmp/main.c
@@ -0,0 +1,19 @@
+/********************************************************
+ * Author		: Hao Ding
+ * Email		: mailtodinghao@gmail.com
+ * Created Time	: Sat 25 Jan 2014 03:06:59 AM CST
+ * Filename		: main.c
+ * Description	: 
+ * ******************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	extern int a;
+	// extern int b;
+	printf("a = %d\n", a);
+	// printf("b = %d\n", b);
+	return 0;
+}
+
